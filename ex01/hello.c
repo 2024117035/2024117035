1c2433a718eb6e9e26418d198daf474e88e739ab
2024117035 <2179498644@qq.com>
2026-04-30 18:11:37 +0800

    第二次提交：为C语言程序新增键盘输入功能，更新README

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char input[100]; // 定义数组存储输入内容
+    printf("请输入内容：");
+    scanf("%s", input); // 读取键盘输入
+    printf("你输入的内容是：%s\n", input);
     return 0;
 }
